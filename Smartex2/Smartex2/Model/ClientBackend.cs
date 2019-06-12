28ca7b21a1361782aa107e1b5648e59533961b1a
SavageLoop <32804644+SavageLoop@users.noreply.github.com>
2019-06-12 19:43:31 +0200

    Update ClientBackend.cs

--- Smartex2/Smartex2/Model/ClientBackend.cs
+++ Smartex2/Smartex2/Model/ClientBackend.cs
@@ -1,207 +1,231 @@
-using Newtonsoft.Json;
-using Smartex.Exception;
-using Smartex.Server;
-using System;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-
-namespace Smartex.Model
+class ClientBackend
 {
-    /**
-     * Klasa odpowiedzialna za zarządzanie danymi logowania użytkownika.
-     * Odpowiada także za rejestracje i logowanie.
-     */
-    class ClientBackend
+    //adres serwera 
+    public static System.Uri api_domain = new System.Uri("https://opclouden.pythonanywhere.com/");
+    private static HttpClientHandler handler = new HttpClientHandler();
+    public static HttpClient client = new HttpClient();
+    private static System.Net.CredentialCache credentialCache = new System.Net.CredentialCache();
+
+    //ustawianie JSON'a
+    static ClientBackend()
     {
-        //adres serwera 
-        public static System.Uri api_domain = new System.Uri("https://opclouden.pythonanywhere.com/");
-        private static HttpClientHandler handler = new HttpClientHandler();
-        public static HttpClient client = new HttpClient();
-        private static System.Net.CredentialCache credentialCache = new System.Net.CredentialCache();
-
-       //ustawianie JSON'a
-        static ClientBackend()
+        handler = new HttpClientHandler();
+        JsonConvert.DefaultSettings = () => new JsonSerializerSettings
         {
-            handler = new HttpClientHandler();
-            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
-            {
-                NullValueHandling = NullValueHandling.Ignore,
-                DefaultValueHandling = DefaultValueHandling.Ignore
-            };
+            NullValueHandling = NullValueHandling.Ignore,
+            DefaultValueHandling = DefaultValueHandling.Ignore
+        };
+    }
+
+
+    /**
+     * @param url - adres pod ktory ma zostac wykonane żadanie GET
+     * @return JSON
+     */
+    internal static async Task<string> GetResponse(string url)
+    {
+        try
+        {
+            if (!IsConnection()) throw new InternetConnectionExcepion();
+
+            HttpResponseMessage response = await client.GetAsync(new Uri(api_domain, url));
+
+            response.EnsureSuccessStatusCode();
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            return responseContent;
         }
-
-        /**
-         * @param url - adres pod ktory ma zostac wykonane żadanie GET
-         * @return JSON
-         */
-        public static async Task<string> GetResponse(string url)
+        catch (InternetConnectionExcepion)
         {
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(new Uri(api_domain, url));
-                response.EnsureSuccessStatusCode();
-
-                string responseContent = await response.Content.ReadAsStringAsync();
-
-                return responseContent;
-            }
-            catch (ArgumentNullException)
-            {
-                throw;
-            }
-            catch (HttpRequestException)
-            {
-                throw;
-            }
-            catch (NullReferenceException)
-            {
-                throw;
-            }
-            catch (System.Exception e)
-            {
-                Console.WriteLine(e.Message);
-                throw new UnknownException();
-            }
-
+            throw;
         }
-
-
-        /**
-         * Zapisuje dane logowania
-        * @param login,hasło używkonika
-        */
-        public static void StroreCredentials(String login, String password)
+        catch (ArgumentNullException)
         {
-            credentialCache.Add(
-                     api_domain,
-                     "Basic",
-                      new System.Net.NetworkCredential(login, password));
-
-            handler.Credentials = credentialCache;
-
-            client = new HttpClient(handler);
+            throw;
         }
-
-        /**
-         * Usuwa dane logowania
-        */
-        public static void RemoveCredentials()
+        catch (HttpRequestException)
         {
-            credentialCache = new System.Net.CredentialCache();
-            handler.Credentials = credentialCache;
-            client = new HttpClient(handler);
+            throw;
         }
-
-        public static async Task Login(String username, String password)
+        catch (NullReferenceException)
         {
-            try
-            {
-                StroreCredentials(username, password);
-                String json = await ClientBackend.GetResponse("/user");
-
-                ServerAnswerRecievedUser userData = JsonConvert.DeserializeObject<ServerAnswerRecievedUser>(await ClientBackend.GetResponse("/user"));
-
-                if (!userData.Status.Equals("success", StringComparison.OrdinalIgnoreCase))
-                {
-                    RemoveCredentials();
-                    throw new LoginException();
-                }
-            }
-            catch (LoginException)
-            {
-                throw new LoginException();
-            }
-            catch (System.Exception)
-            {
-                throw new UnknownException();
-            }
+            throw;
         }
-
-        /**
-         * Rejestracja użytkownika
-         * @param UserPersonalInfo - wszystkie potrzebne dane do rejestracji
-         */
-        public static async Task RegisterUser(UserPersonalInfo userPersonalInfo)
+        catch (System.Exception e)
         {
-            try
-            {
-                string json = JsonConvert.SerializeObject(userPersonalInfo);
-
-                var response = await ClientBackend.client.PostAsync((ClientBackend.api_domain + "/user"),
-                    new StringContent(json, Encoding.UTF8, "application/json"));
-
-                string responseContent = await response.Content.ReadAsStringAsync();
-
-                ServerFeedback serverFeedback = JsonConvert.DeserializeObject<ServerFeedback>(responseContent);
-
-                if (serverFeedback.Status.Equals("success", StringComparison.OrdinalIgnoreCase))
-                {
-                    StroreCredentials(userPersonalInfo.Login, userPersonalInfo.Password);
-                }
-                else
-                {
-                    throw new DataFormatException();
-                }
-            }
-            catch (DataFormatException)
-            {
-                throw;
-            }
-            catch (ArgumentNullException)
-            {
-                throw;
-            }
-            catch (HttpRequestException)
-            {
-                throw;
-            }
-            catch (System.Exception)
-            {
-                throw new UnknownException();
-            }
-        }
-       
-        public static void Logout()
-        {
-            try
-            {
-                RemoveCredentials();
-                App.CurrentUser = null;
-            }
-            catch(System.Exception e)
-            {
-                throw new UnknownException();
-            }
-        }
-
-        
-        /**
-        * Pobiera informacje na temat konkretnego użytkownika
-        * @param userID - unikalny identyfikator użytkownika
-        * @return ServerAnswerRecievedUser 
-        */
-        public static async Task<ServerAnswerRecievedUser> GetUser(int userID)
-        {
-            try
-            {
-                String json = await ClientBackend.GetResponse("/user");
-                ServerAnswerRecievedUser data = JsonConvert.DeserializeObject<ServerAnswerRecievedUser>(await ClientBackend.GetResponse("/user/" + userID));
-                return data;
-            }
-            catch (System.Exception)
-            {
-                throw new UnknownException();
-            }
-
-        }
-        
-        ~ClientBackend()
-        {
-            handler.Dispose();
-            client.Dispose();
+            throw new UnknownException();
         }
 
     }
+
+
+    /**
+     * Zapisuje dane logowania
+    * @param login,hasło używkonika
+    */
+    public static void StroreCredentials(String login, String password)
+    {
+        credentialCache.Add(
+                 api_domain,
+                 "Basic",
+                  new System.Net.NetworkCredential(login, password));
+
+        handler.Credentials = credentialCache;
+
+        client = new HttpClient(handler);
+    }
+
+    /**
+     * Usuwa dane logowania
+    */
+    public static void RemoveCredentials()
+    {
+        credentialCache = new System.Net.CredentialCache();
+        handler.Credentials = credentialCache;
+        client = new HttpClient(handler);
+    }
+
+    public static async Task Login(String username, String password)
+    {
+        try
+        {
+            if (!IsConnection()) throw new InternetConnectionExcepion();
+
+            StroreCredentials(username, password);
+            String json = await ClientBackend.GetResponse("/user");
+
+            ServerAnswerRecievedUser userData = JsonConvert.DeserializeObject<ServerAnswerRecievedUser>(await ClientBackend.GetResponse("/user"));
+
+            if (!userData.Status.Equals("success", StringComparison.OrdinalIgnoreCase))
+            {
+                RemoveCredentials();
+                throw new LoginException();
+            }
+        }
+        catch (InternetConnectionExcepion)
+        {
+            throw;
+        }
+        catch (LoginException)
+        {
+            throw new LoginException();
+        }
+        catch (System.Exception)
+        {
+            throw new UnknownException();
+        }
+    }
+
+
+
+
+    /**
+     * Rejestracja użytkownika
+     * @param UserPersonalInfo - wszystkie potrzebne dane do rejestracji
+     */
+    public static async Task RegisterUser(UserPersonalInfo userPersonalInfo)
+    {
+        try
+        {
+            if (!IsConnection()) throw new InternetConnectionExcepion();
+
+            string json = JsonConvert.SerializeObject(userPersonalInfo);
+
+            var response = await ClientBackend.client.PostAsync((ClientBackend.api_domain + "/user"),
+                new StringContent(json, Encoding.UTF8, "application/json"));
+
+            string responseContent = await response.Content.ReadAsStringAsync();
+
+            ServerFeedback serverFeedback = JsonConvert.DeserializeObject<ServerFeedback>(responseContent);
+
+            if (serverFeedback.Status.Equals("success", StringComparison.OrdinalIgnoreCase))
+            {
+                StroreCredentials(userPersonalInfo.Login, userPersonalInfo.Password);
+            }
+            else
+            {
+                throw new DataFormatException();
+            }
+        }
+        catch (InternetConnectionExcepion)
+        {
+            throw;
+        }
+        catch (DataFormatException)
+        {
+            throw;
+        }
+        catch (ArgumentNullException)
+        {
+            throw;
+        }
+        catch (HttpRequestException)
+        {
+            throw;
+        }
+        catch (System.Exception)
+        {
+            throw new UnknownException();
+        }
+    }
+
+    public static void Logout()
+    {
+        try
+        {
+            RemoveCredentials();
+        }
+        catch (System.Exception e)
+        {
+            throw new UnknownException();
+        }
+    }
+
+
+    /**
+    * Pobiera informacje na temat konkretnego użytkownika
+    * @param userID - unikalny identyfikator użytkownika
+    * @return ServerAnswerRecievedUser 
+    */
+    public static async Task<ServerAnswerRecievedUser> GetUser(int userID)
+    {
+        try
+        {
+            String json = await ClientBackend.GetResponse("/user");
+            ServerAnswerRecievedUser data = JsonConvert.DeserializeObject<ServerAnswerRecievedUser>(await ClientBackend.GetResponse("/user/" + userID));
+            return data;
+        }
+        catch (System.Exception)
+        {
+            throw new UnknownException();
+        }
+
+    }
+
+    ~ClientBackend()
+    {
+        handler.Dispose();
+        client.Dispose();
+    }
+
+
+    public static bool IsConnection()
+    {
+        try
+        {
+            Ping myPing = new Ping();
+            String host = "google.com";
+            byte[] buffer = new byte[32];
+            int timeout = 1000;
+            PingOptions pingOptions = new PingOptions();
+            PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+            return (reply.Status == IPStatus.Success);
+        }
+        catch (Exception)
+        {
+            throw new InternetConnectionExcepion();
+        }
+    }
+
 }
